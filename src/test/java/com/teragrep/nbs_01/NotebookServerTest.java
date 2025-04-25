c09fff718d2f402f736e13a71165e7ab3fae05bf
Laukkala <140611897+Laukkala@users.noreply.github.com>
2025-04-25 13:05:35 +0300

    Added simple tests for connecting to the server via HTTP and WebSocket

--- src/test/java/com/teragrep/nbs_01/NotebookServerTest.java
+++ src/test/java/com/teragrep/nbs_01/NotebookServerTest.java
@@ -5,7 +5,9 @@ import org.eclipse.jetty.websocket.client.WebSocketClient;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.net.HttpURLConnection;
 import java.net.URI;
+import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -13,22 +15,35 @@ public class NotebookServerTest
 {
     private final int serverPort = 8080;
     private final String serverAddress = "localhost:"+serverPort;
-    private final Path notebookDirectory = Paths.get("target/notebook");
+    private final Path notebookDirectory = Paths.get("target/notebooks");
     private final Configuration testConfiguration = new Configuration(notebookDirectory,serverPort);
 
     public NotebookServerTest(){
         NotebookServer server = new NotebookServer(testConfiguration);
         server.start();
     }
-
     @Test
-    public void connectTest(){
+    // Assert that a simple HTTP request to an existing endpoint results in return code 200 OK
+    public void httpConnectTest(){
+        Assertions.assertDoesNotThrow(()->{
+            URL serverURL = new URL("http://"+serverAddress+"/notebook/hello");
+            HttpURLConnection connection = (HttpURLConnection) serverURL.openConnection();
+            int status = connection.getResponseCode();
+            connection.disconnect();
+            Assertions.assertEquals(200,status);
+        });
+    }
+    @Test
+    // Assert that A WebSocket connection is established, and that it is closed after a call to WebSocketClient.close()
+    public void webSocketConnectTest(){
         Assertions.assertDoesNotThrow(()->{
             URI serverURI = URI.create("ws://"+serverAddress+"/notebook/list");
             WebSocketClient webSocketClient = new WebSocketClient(new HttpClient());
             webSocketClient.start();
             TestWebSocketConnection client = new TestWebSocketConnection(webSocketClient,serverURI);
-            client.sendText("hello");
+            Assertions.assertEquals(1,webSocketClient.getOpenSessions().size());
+            webSocketClient.close();
+            Assertions.assertEquals(0,webSocketClient.getOpenSessions().size());
         });
     }
 }
