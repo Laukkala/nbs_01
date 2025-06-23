dfaf4ff864adc46f8d429e19c12140bed84ca58d
Laukkala <140611897+Laukkala@users.noreply.github.com>
2025-06-23 13:06:35 +0300

    Re-wrote httpCopyParagraphTest

--- src/test/java/com/teragrep/nbs_01/ParagraphServletTest.java
+++ src/test/java/com/teragrep/nbs_01/ParagraphServletTest.java
@@ -293,54 +293,55 @@ public class ParagraphServletTest extends AbstractNotebookServerTest {
     // Copying is not an atomic operation. You must first create a new paragraph, then update it with the output of the source paragraph.
     @Test
     public void httpCopyParagraphTest() {
-        String notebookId = "2A94M5J1Z";
-        String sourceParagraphId = "20150213-231621_168813393";
-        String copyParagraphId = "copyParagraphId";
-        String notebookPath = Paths
-                .get("my_folder_2A94M5J1D", "my_second_folder_2A94M5J2D", "my_note1_" + notebookId + ".zpln")
-                .toString();
+
+        String newParagraphId = "2025-01-01-021311-132-133";
 
         // Make an HTTP GET request to /notebook/{path/to/notebook/}/paragraph/{paragraphId}
         Response getResponse = Assertions
                 .assertDoesNotThrow(
                         () -> makeHttpGETRequest(
-                                "http://" + serverAddress() + "/notebook/" + notebookPath + "/paragraph/"
-                                        + sourceParagraphId
+                                "http://" + serverAddress() + "/notebook/" + notebookPath + "/paragraph/" + firstParagraphId
                         )
                 );
+        // Assert that a GET request is responded to with the response code 200 OK
         Assertions.assertEquals(HttpStatus.OK_200, getResponse.status());
-        JsonObject paragraph = Json
-                .createReader(new StringReader(getResponse.body().getString("message")))
-                .readObject();
-        String text = paragraph.getJsonObject("script").getString("text");
+
+        JsonObject sourceParagraph = Json.createReader(new StringReader(getResponse.body().getString("message"))).readObject();
 
         // Make an HTTP PUT request to /notebook/{path/to/notebook/}/paragraph/{paragraphId}
+        String putRequestBody = Json.createObjectBuilder().build().toString();
         Response putResponse = Assertions
                 .assertDoesNotThrow(
                         () -> makeHttpPUTRequest(
-                                "http://" + serverAddress() + "/notebook/" + notebookPath + "/paragraph/"
-                                        + copyParagraphId,
-                                "{}"
+                                "http://" + serverAddress() + "/notebook/" + notebookPath + "/paragraph/" + newParagraphId,putRequestBody
                         )
                 );
+        // Assert that a GET request is responded to with the response code 201 CREATED
         Assertions.assertEquals(HttpStatus.CREATED_201, putResponse.status());
 
         // Make an HTTP POST request to /notebook/{path/to/notebook/}/paragraph/{paragraphId}
+        String newParagraphTitle = sourceParagraph.getString("title");
+        String newParagraphText = sourceParagraph.getJsonObject("script").getString("text");
+        String postRequestBody = Json.createObjectBuilder().add("title",newParagraphTitle).add("text",newParagraphText).build().toString();
+        String expectedParagraphContent = Json.createObjectBuilder().add("id",newParagraphId)
+                .add("title",newParagraphTitle)
+                .add("script",Json.createObjectBuilder()
+                        .add("text",newParagraphText))
+                .build().toString();
+
         Response postResponse = Assertions
                 .assertDoesNotThrow(
                         () -> makeHttpPOSTRequest(
-                                "http://" + serverAddress() + "/notebook/" + notebookPath + "/paragraph/"
-                                        + copyParagraphId,
-                                "{\"text\":" + text + "}"
+                                "http://" + serverAddress() + "/notebook/" + notebookPath + "/paragraph/" + newParagraphId,postRequestBody
                         )
                 );
+        // Assert that a GET request is responded to with the response code 201 CREATED
         Assertions.assertEquals(HttpStatus.OK_200, postResponse.status());
 
-        // Assert that the copied paragraph exists in the correct notebook's saved file
-        String expectedFileContent = "{\"id\":\"2A94M5J1Z\",\"name\":\"\",\"config\":{},\"paragraphs\":[{\"id\":\"20150213-230422_1600658137\",\"title\":\"\",\"script\":{\"text\":\"{\\\"text\\\":\\\"\\\\\\\"%test \\\\\\\\nselect age, count(1) value \\\\\\\\nfrom bank \\\\\\\\nwhere marital=\\\\\\\\\\\\\\\"${marital=single,single|divorced|married}\\\\\\\\\\\\\\\" \\\\\\\\ngroup by age \\\\\\\\norder by age\\\\\\\"\\\"}\"}},{\"id\":\"20150213-230428_1231780373\",\"title\":\"\",\"script\":{\"text\":\"{\\\"text\\\":\\\"\\\\\\\"%test\\\\\\\\n## Congratulations, it's done.\\\\\\\\n##### You can create your own notebook in 'Notebook' menu. Good luck!\\\\\\\"\\\"}\"}},{\"id\":\"20150326-214658_12335843\",\"title\":\"\",\"script\":{\"text\":\"{\\\"text\\\":\\\"\\\\\\\"%test\\\\\\\\n\\\\\\\\nAbout bank data\\\\\\\\n\\\\\\\\n```\\\\\\\\nCitation Request:\\\\\\\\n  This dataset is public available for research. The details are described in [Moro et al., 2011]. \\\\\\\\n  Please include this citation if you plan to use this database:\\\\\\\\n\\\\\\\\n  [Moro et al., 2011] S. Moro, R. Laureano and P. Cortez. Using Data Mining for Bank Direct Marketing: An Application of the CRISP-DM Methodology. \\\\\\\\n  In P. Novais et al. (Eds.), Proceedings of the European Simulation and Modelling Conference - ESM'2011, pp. 117-121, Guimarães, Portugal, October, 2011. EUROSIS.\\\\\\\\n\\\\\\\\n  Available at: [pdf] http://hdl.handle.net/1822/14838\\\\\\\\n                [bib] http://www3.dsi.uminho.pt/pcortez/bib/2011-esm-1.txt\\\\\\\\n```\\\\\\\"\\\"}\"}},{\"id\":\"20150210-015302_1492795503\",\"title\":\"\",\"script\":{\"text\":\"{\\\"text\\\":\\\"\\\\\\\"%test \\\\\\\\nselect age, count(1) value\\\\\\\\nfrom bank \\\\\\\\nwhere age < 30 \\\\\\\\ngroup by age \\\\\\\\norder by age\\\\\\\"\\\"}\"}},{\"id\":\"20150210-015259_1403135953\",\"title\":\"Load data into table\",\"script\":{\"text\":\"{\\\"text\\\":\\\"\\\\\\\"%test import org.apache.commons.io.IOUtils\\\\\\\\nimport java.net.URL\\\\\\\\nimport java.nio.charset.Charset\\\\\\\\n\\\\\\\\n// Zeppelin creates and injects sc (SparkContext) and sqlContext (HiveContext or SqlContext)\\\\\\\\n// So you don't need create them manually\\\\\\\\n\\\\\\\\n// load bank data\\\\\\\\nval bankText = sc.parallelize(\\\\\\\\n    IOUtils.toString(\\\\\\\\n        new URL(\\\\\\\\\\\\\\\"https://s3.amazonaws.com/apache-zeppelin/tutorial/bank/bank.csv\\\\\\\\\\\\\\\"),\\\\\\\\n        Charset.forName(\\\\\\\\\\\\\\\"utf8\\\\\\\\\\\\\\\")).split(\\\\\\\\\\\\\\\"\\\\\\\\\\\\\\\\n\\\\\\\\\\\\\\\"))\\\\\\\\n\\\\\\\\ncase class Bank(age: Integer, job: String, marital: String, education: String, balance: Integer)\\\\\\\\n\\\\\\\\nval bank = bankText.map(s => s.split(\\\\\\\\\\\\\\\";\\\\\\\\\\\\\\\")).filter(s => s(0) != \\\\\\\\\\\\\\\"\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\"age\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\"\\\\\\\\\\\\\\\").map(\\\\\\\\n    s => Bank(s(0).toInt, \\\\\\\\n            s(1).replaceAll(\\\\\\\\\\\\\\\"\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\"\\\\\\\\\\\\\\\", \\\\\\\\\\\\\\\"\\\\\\\\\\\\\\\"),\\\\\\\\n            s(2).replaceAll(\\\\\\\\\\\\\\\"\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\"\\\\\\\\\\\\\\\", \\\\\\\\\\\\\\\"\\\\\\\\\\\\\\\"),\\\\\\\\n            s(3).replaceAll(\\\\\\\\\\\\\\\"\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\"\\\\\\\\\\\\\\\", \\\\\\\\\\\\\\\"\\\\\\\\\\\\\\\"),\\\\\\\\n            s(5).replaceAll(\\\\\\\\\\\\\\\"\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\"\\\\\\\\\\\\\\\", \\\\\\\\\\\\\\\"\\\\\\\\\\\\\\\").toInt\\\\\\\\n        )\\\\\\\\n).toDF()\\\\\\\\nbank.registerTempTable(\\\\\\\\\\\\\\\"bank\\\\\\\\\\\\\\\")\\\\\\\"\\\"}\"}},{\"id\":\"20150213-231621_168813393\",\"title\":\"\",\"script\":{\"text\":\"{\\\"text\\\":\\\"\\\\\\\"%test\\\\\\\\n## Welcome to Zeppelin.\\\\\\\\n##### This is a live tutorial, you can run the code yourself. (Shift-Enter to Run)\\\\\\\"\\\"}\"}},{\"id\":\"20150703-133047_853701097\",\"title\":\"\",\"script\":{\"text\":\"{\\\"text\\\":\\\"\\\"}\"}},{\"id\":\"20150212-145404_867439529\",\"title\":\"\",\"script\":{\"text\":\"{\\\"text\\\":\\\"\\\\\\\"%test \\\\\\\\nselect age, count(1) value \\\\\\\\nfrom bank \\\\\\\\nwhere age < ${maxAge=30} \\\\\\\\ngroup by age \\\\\\\\norder by age\\\\\\\"\\\"}\"}},{\"id\":\"copyParagraphId\",\"title\":\"\",\"script\":{\"text\":\"%test\\n## Welcome to Zeppelin.\\n##### This is a live tutorial, you can run the code yourself. (Shift-Enter to Run)\"}}]}";
-        String fileContent = Assertions
-                .assertDoesNotThrow(() -> Files.readString(Paths.get(notebookDirectory().toString(), notebookPath)));
-        Assertions.assertEquals(expectedFileContent, fileContent);
+        // Assert that the created paragraph is contained within the saved file of the notebook
+        String fileContents = Assertions.assertDoesNotThrow(()->Files.readString(Paths.get(notebookDirectory().toString(),notebookPath.toString())));
+        Assertions.assertTrue(fileContents.contains(expectedParagraphContent));
+        System.out.println(fileContents);
     }
 
 }
