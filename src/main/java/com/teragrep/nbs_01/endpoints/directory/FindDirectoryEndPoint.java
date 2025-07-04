3b30afc467d333c3453ca554218ece1b8ec7d7fb
Laukkala <140611897+Laukkala@users.noreply.github.com>
2025-07-04 09:54:38 +0300

    Added a test for FindDirectoryEndPoint FindDirectoryEndpoint now uses ExceptionResponses

--- src/main/java/com/teragrep/nbs_01/endpoints/directory/FindDirectoryEndPoint.java
+++ src/main/java/com/teragrep/nbs_01/endpoints/directory/FindDirectoryEndPoint.java
@@ -50,6 +50,7 @@ import com.teragrep.nbs_01.exceptions.MalformedRequestException;
 import com.teragrep.nbs_01.repository.Directory;
 import com.teragrep.nbs_01.repository.ZeppelinFile;
 import com.teragrep.nbs_01.requests.Request;
+import com.teragrep.nbs_01.responses.ExceptionResponse;
 import com.teragrep.nbs_01.responses.SimpleResponse;
 import com.teragrep.nbs_01.responses.JsonResponse;
 import jakarta.json.JsonObject;
@@ -85,13 +86,13 @@ public class FindDirectoryEndPoint implements EndPoint {
             }
         }
         catch (FileNotFoundException fileNotFoundException) {
-            return new SimpleResponse(HttpStatus.BAD_REQUEST_400, "Directory not found!");
+            return new ExceptionResponse(HttpStatus.BAD_REQUEST_400, fileNotFoundException);
         }
         catch (IOException ioException) {
-            return new SimpleResponse(HttpStatus.INTERNAL_SERVER_ERROR_500, "An error occurred");
+            return new ExceptionResponse(HttpStatus.INTERNAL_SERVER_ERROR_500, ioException);
         }
         catch (MalformedRequestException malformedRequestException) {
-            return new SimpleResponse(HttpStatus.BAD_REQUEST_400, "Malformed request:\n" + malformedRequestException);
+            return new ExceptionResponse(HttpStatus.BAD_REQUEST_400, malformedRequestException);
         }
     }
 }
