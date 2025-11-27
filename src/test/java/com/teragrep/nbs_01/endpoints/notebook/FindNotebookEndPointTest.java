3856b5556b6954e6c0fee5cf55a30be830da7dad
Laukkala <140611897+Laukkala@users.noreply.github.com>
2025-11-27 12:20:48 +0200

    Separated paragraphs from FindNotebookEndPointTest to separate assertions to allow for paragraphs to be in any order.

--- src/test/java/com/teragrep/nbs_01/endpoints/notebook/FindNotebookEndPointTest.java
+++ src/test/java/com/teragrep/nbs_01/endpoints/notebook/FindNotebookEndPointTest.java
@@ -66,7 +66,15 @@ public class FindNotebookEndPointTest extends AbstractNotebookServerTest {
 
     private final Path notebookPath = Paths
             .get("my_folder_2A94M5J1D/my_second_folder_2A94M5J2D/my_note1_2A94M5J1Z.zpln");
-    private final String expectedFileContent = "{\"name\":\"my_note1\",\"config\":{},\"paragraphs\":[{\"id\":\"20150213-231621_168813393\",\"title\":\"\",\"script\":{\"text\":\"%test\\n## Welcome to Zeppelin.\\n##### This is a live tutorial, you can run the code yourself. (Shift-Enter to Run)\"}},{\"id\":\"20150210-015259_1403135953\",\"title\":\"Load data into table\",\"script\":{\"text\":\"%test import org.apache.commons.io.IOUtils\\nimport java.net.URL\\nimport java.nio.charset.Charset\\n\\n// Zeppelin creates and injects sc (SparkContext) and sqlContext (HiveContext or SqlContext)\\n// So you don't need create them manually\\n\\n// load bank data\\nval bankText = sc.parallelize(\\n    IOUtils.toString(\\n        new URL(\\\"https://s3.amazonaws.com/apache-zeppelin/tutorial/bank/bank.csv\\\"),\\n        Charset.forName(\\\"utf8\\\")).split(\\\"\\\\n\\\"))\\n\\ncase class Bank(age: Integer, job: String, marital: String, education: String, balance: Integer)\\n\\nval bank = bankText.map(s => s.split(\\\";\\\")).filter(s => s(0) != \\\"\\\\\\\"age\\\\\\\"\\\").map(\\n    s => Bank(s(0).toInt, \\n            s(1).replaceAll(\\\"\\\\\\\"\\\", \\\"\\\"),\\n            s(2).replaceAll(\\\"\\\\\\\"\\\", \\\"\\\"),\\n            s(3).replaceAll(\\\"\\\\\\\"\\\", \\\"\\\"),\\n            s(5).replaceAll(\\\"\\\\\\\"\\\", \\\"\\\").toInt\\n        )\\n).toDF()\\nbank.registerTempTable(\\\"bank\\\")\"}},{\"id\":\"20150210-015302_1492795503\",\"title\":\"\",\"script\":{\"text\":\"%test \\nselect age, count(1) value\\nfrom bank \\nwhere age < 30 \\ngroup by age \\norder by age\"}},{\"id\":\"20150212-145404_867439529\",\"title\":\"\",\"script\":{\"text\":\"%test \\nselect age, count(1) value \\nfrom bank \\nwhere age < ${maxAge=30} \\ngroup by age \\norder by age\"}},{\"id\":\"20150213-230422_1600658137\",\"title\":\"\",\"script\":{\"text\":\"%test \\nselect age, count(1) value \\nfrom bank \\nwhere marital=\\\"${marital=single,single|divorced|married}\\\" \\ngroup by age \\norder by age\"}},{\"id\":\"20150213-230428_1231780373\",\"title\":\"\",\"script\":{\"text\":\"%test\\n## Congratulations, it's done.\\n##### You can create your own notebook in 'Notebook' menu. Good luck!\"}},{\"id\":\"20150326-214658_12335843\",\"title\":\"\",\"script\":{\"text\":\"%test\\n\\nAbout bank data\\n\\n```\\nCitation Request:\\n  This dataset is public available for research. The details are described in [Moro et al., 2011]. \\n  Please include this citation if you plan to use this database:\\n\\n  [Moro et al., 2011] S. Moro, R. Laureano and P. Cortez. Using Data Mining for Bank Direct Marketing: An Application of the CRISP-DM Methodology. \\n  In P. Novais et al. (Eds.), Proceedings of the European Simulation and Modelling Conference - ESM'2011, pp. 117-121, Guimarães, Portugal, October, 2011. EUROSIS.\\n\\n  Available at: [pdf] http://hdl.handle.net/1822/14838\\n                [bib] http://www3.dsi.uminho.pt/pcortez/bib/2011-esm-1.txt\\n```\"}},{\"id\":\"20150703-133047_853701097\",\"title\":\"\",\"script\":{\"text\":\"\"}}]}";
+    private final String expectedFileContent = "\"name\":\"my_note1\",\"config\":{}";
+    private final String expectedParagraph1 = "{\"id\":\"20150213-231621_168813393\",\"title\":\"\",\"script\":{\"text\":\"%test\\n## Welcome to Zeppelin.\\n##### This is a live tutorial, you can run the code yourself. (Shift-Enter to Run)\"}}";
+    private final String expectedParagraph2 = "{\"id\":\"20150210-015259_1403135953\",\"title\":\"Load data into table\",\"script\":{\"text\":\"%test import org.apache.commons.io.IOUtils\\nimport java.net.URL\\nimport java.nio.charset.Charset\\n\\n// Zeppelin creates and injects sc (SparkContext) and sqlContext (HiveContext or SqlContext)\\n// So you don't need create them manually\\n\\n// load bank data\\nval bankText = sc.parallelize(\\n    IOUtils.toString(\\n        new URL(\\\"https://s3.amazonaws.com/apache-zeppelin/tutorial/bank/bank.csv\\\"),\\n        Charset.forName(\\\"utf8\\\")).split(\\\"\\\\n\\\"))\\n\\ncase class Bank(age: Integer, job: String, marital: String, education: String, balance: Integer)\\n\\nval bank = bankText.map(s => s.split(\\\";\\\")).filter(s => s(0) != \\\"\\\\\\\"age\\\\\\\"\\\").map(\\n    s => Bank(s(0).toInt, \\n            s(1).replaceAll(\\\"\\\\\\\"\\\", \\\"\\\"),\\n            s(2).replaceAll(\\\"\\\\\\\"\\\", \\\"\\\"),\\n            s(3).replaceAll(\\\"\\\\\\\"\\\", \\\"\\\"),\\n            s(5).replaceAll(\\\"\\\\\\\"\\\", \\\"\\\").toInt\\n        )\\n).toDF()\\nbank.registerTempTable(\\\"bank\\\")\"}}";
+    private final String expectedParagraph3 = "{\"id\":\"20150210-015302_1492795503\",\"title\":\"\",\"script\":{\"text\":\"%test \\nselect age, count(1) value\\nfrom bank \\nwhere age < 30 \\ngroup by age \\norder by age\"}}";
+    private final String expectedParagraph4 = "{\"id\":\"20150212-145404_867439529\",\"title\":\"\",\"script\":{\"text\":\"%test \\nselect age, count(1) value \\nfrom bank \\nwhere age < ${maxAge=30} \\ngroup by age \\norder by age\"}}";
+    private final String expectedParagraph5 = "{\"id\":\"20150213-230422_1600658137\",\"title\":\"\",\"script\":{\"text\":\"%test \\nselect age, count(1) value \\nfrom bank \\nwhere marital=\\\"${marital=single,single|divorced|married}\\\" \\ngroup by age \\norder by age\"}}";
+    private final String expectedParagraph6 = "{\"id\":\"20150213-230428_1231780373\",\"title\":\"\",\"script\":{\"text\":\"%test\\n## Congratulations, it's done.\\n##### You can create your own notebook in 'Notebook' menu. Good luck!\"}}";
+    private final String expectedParagraph7 = "{\"id\":\"20150326-214658_12335843\",\"title\":\"\",\"script\":{\"text\":\"%test\\n\\nAbout bank data\\n\\n```\\nCitation Request:\\n  This dataset is public available for research. The details are described in [Moro et al., 2011]. \\n  Please include this citation if you plan to use this database:\\n\\n  [Moro et al., 2011] S. Moro, R. Laureano and P. Cortez. Using Data Mining for Bank Direct Marketing: An Application of the CRISP-DM Methodology. \\n  In P. Novais et al. (Eds.), Proceedings of the European Simulation and Modelling Conference - ESM'2011, pp. 117-121, Guimarães, Portugal, October, 2011. EUROSIS.\\n\\n  Available at: [pdf] http://hdl.handle.net/1822/14838\\n                [bib] http://www3.dsi.uminho.pt/pcortez/bib/2011-esm-1.txt\\n```\"}}";
+    private final String expectedParagraph8 = "{\"id\":\"20150703-133047_853701097\",\"title\":\"\",\"script\":{\"text\":\"\"}}";
 
     @BeforeEach
     private void setUp() {
@@ -92,7 +100,39 @@ public class FindNotebookEndPointTest extends AbstractNotebookServerTest {
         Assertions.assertEquals(expectedContentTypeHeader.toString(), response.headers().get(1).toString());
         Assertions
                 .assertDoesNotThrow(
-                        () -> Assertions.assertEquals(expectedFileContent, response.body().asString().strip().toString())
+                        () -> Assertions.assertTrue(response.body().asString().strip().contains(expectedFileContent))
+                );
+        Assertions
+                .assertDoesNotThrow(
+                        () -> Assertions.assertTrue(response.body().asString().strip().contains(expectedParagraph1))
+                );
+        Assertions
+                .assertDoesNotThrow(
+                        () -> Assertions.assertTrue(response.body().asString().strip().contains(expectedParagraph2))
+                );
+        Assertions
+                .assertDoesNotThrow(
+                        () -> Assertions.assertTrue(response.body().asString().strip().contains(expectedParagraph3))
+                );
+        Assertions
+                .assertDoesNotThrow(
+                        () -> Assertions.assertTrue(response.body().asString().strip().contains(expectedParagraph4))
+                );
+        Assertions
+                .assertDoesNotThrow(
+                        () -> Assertions.assertTrue(response.body().asString().strip().contains(expectedParagraph5))
+                );
+        Assertions
+                .assertDoesNotThrow(
+                        () -> Assertions.assertTrue(response.body().asString().strip().contains(expectedParagraph6))
+                );
+        Assertions
+                .assertDoesNotThrow(
+                        () -> Assertions.assertTrue(response.body().asString().strip().contains(expectedParagraph7))
+                );
+        Assertions
+                .assertDoesNotThrow(
+                        () -> Assertions.assertTrue(response.body().asString().strip().contains(expectedParagraph8))
                 );
     }
 
